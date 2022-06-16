b2fb01362c5c21f13554366b3ca35c4f827a08cd
ArpitAgravat <92981201+ArpitAgravat@users.noreply.github.com>
2022-06-17 00:41:57 +0530

    Update Frame1.java

--- Frame1.java
+++ Frame1.java
@@ -4,7 +4,7 @@ import java.awt.AWTEvent.*;
 import java.util.regex.*;
 class Frame1 extends Frame
 {
-	Button b1,b2,b3,b4,b5;
+	Button b1,b2,b3,b4,b5,b6;
 	Label l1;
 	Font f1;
 	MyActionListener L1 = new MyActionListener(this);
@@ -16,6 +16,7 @@ class Frame1 extends Frame
 		
 		b1 = new Button("2 variable k map");
 		b2 = new Button("3 variable k map");
+		b6 = new Button("4 variable k map");
 		b3 = new Button("enter");
 		b4 = new Button("clear");
 		b5 = new Button("exit");
@@ -26,9 +27,10 @@ class Frame1 extends Frame
 		
 		b1.setBounds(500,300,400,50);
 		b2.setBounds(500,400,400,50);
-		b3.setBounds(350,500,75,55);
-		b4.setBounds(625,500,75,55);
-		b5.setBounds(925,500,75,55);
+		b6.setBounds(500,500,400,50);
+		b3.setBounds(350,600,75,55);
+		b4.setBounds(625,600,75,55);
+		b5.setBounds(925,600,75,55);
 		
 		
 		Font f1 = new Font("Arial",Font.BOLD,20);
@@ -40,21 +42,20 @@ class Frame1 extends Frame
 		b3.setFont(f1);
 		b4.setFont(f1);
 		b5.setFont(f1);
-		
+		b5.setFont(f1);
+		b6.setFont(f1);
 		
 		add(l1);
 		add(b1);
 		add(b2);
-		add(b3);
-		add(b4);
-		add(b5);
 		
+		add(b6);
 		b1.addActionListener(L1);
 		b2.addActionListener(L1);
 		b3.addActionListener(L1);
 		b4.addActionListener(L1);
 		b5.addActionListener(L1);
-		
+		b6.addActionListener(L1);
 		addWindowListener(new WindowAdapter()
 		{
 			public void windowClosing(WindowEvent e)
