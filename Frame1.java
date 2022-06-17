b57438d8b28be8491d03c12fc43bcd5dab0abd6f
ArpitAgravat <92981201+ArpitAgravat@users.noreply.github.com>
2022-06-17 18:47:00 +0530

    Update Frame1.java

--- Frame1.java
+++ Frame1.java
@@ -1,76 +1,128 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.AWTEvent.*;
+
 import java.util.regex.*;
-class Frame1 extends Frame
+import java.io.*;
+import javax.swing.*;
+import java.lang.*;
+class MyActionListener implements ActionListener
 {
-	Button b1,b2,b3,b4,b5,b6;
-	Label l1;
-	Font f1;
-	MyActionListener L1 = new MyActionListener(this);
-	Frame1()
+	String temp,temp2;
+	Boolean f1,f2,f3,f4,f5,f6;
+	Frame1 fm1;
+	MyFrame fm;
+	Frame2 fm2;
+	Frame3 fm3;
+	Frame4 fm4;
+	MyActionListener(MyFrame i)
 	{
-		super("variable");//doubt
-		this.setBounds(0,0,1000,800);
-		setLayout(null);
+		this.fm = i;
+	}
+	MyActionListener(Frame1 m)
+	{
+		this.fm1 = m;
+	}
+	MyActionListener(Frame2 g)
+	{
+		this.fm2 = g;
+	}
+	MyActionListener(Frame3 f)
+	{
+		this.fm3 = f;
+	}
+	MyActionListener(Frame4 h)
+	{
+		this.fm4 = h;
+	}
+	public void actionPerformed(ActionEvent e)
+	{
 		
-		b1 = new Button("2 variable k map");
-		b2 = new Button("3 variable k map");
-		b6 = new Button("4 variable k map");
-		b3 = new Button("enter");
-		b4 = new Button("clear");
-		b5 = new Button("exit");
-		l1 = new Label("which type of k map you want?");
-		
-		
-		l1.setBounds(500,200,600,75);
-		
-		b1.setBounds(500,300,400,50);
-		b2.setBounds(500,400,400,50);
-		b6.setBounds(500,500,400,50);
-		b3.setBounds(350,600,75,55);
-		b4.setBounds(625,600,75,55);
-		b5.setBounds(925,600,75,55);
-		
-		
-		Font f1 = new Font("Arial",Font.BOLD,20);
-		
-		
-		l1.setFont(f1);
-		b1.setFont(f1);
-		b2.setFont(f1);
-		b3.setFont(f1);
-		b4.setFont(f1);
-		b5.setFont(f1);
-		b5.setFont(f1);
-		b6.setFont(f1);
-		
-		add(l1);
-		add(b1);
-		add(b2);
-		
-		add(b6);
-		b1.addActionListener(L1);
-		b2.addActionListener(L1);
-		b3.addActionListener(L1);
-		b4.addActionListener(L1);
-		b5.addActionListener(L1);
-		b6.addActionListener(L1);
-		addWindowListener(new WindowAdapter()
+		if(e.getActionCommand().equals("Confirm"))
 		{
-			public void windowClosing(WindowEvent e)
+			
+			fm1 = new Frame1();
+			
+			this.fm1.setVisible(true);
+		}
+		if(e.getActionCommand().equals("2 variable k map"))
+		{
+			fm2 = new Frame2();
+			this.fm2.setVisible(true);
+			this.fm1.setVisible(false);
+			
+			
+			
+			
+		}
+		
+		if(e.getActionCommand().equals("3 variable k map"))
+		{
+			fm3 = new Frame3();
+			this.fm3.setVisible(true);
+				
+		}
+		if(e.getActionCommand().equals("4 variable k map"))
+		{
+			fm4 = new Frame4();
+			
+			this.fm4.setVisible(true);
+			
+			
+		}
+		
+			if(e.getSource()== this.fm2.b5)
 			{
-				System.exit(0);
+				
+				temp = this.fm2.b5.getLabel();
+				
+				if(temp== "X")
+				{
+					f1 = true;
+					f2 = false;
+					f3 = false;
+					
+				}
+				else if(temp== "1")
+				{
+					f2 = true;
+					f3 = false;
+					f1 = false;
+					
+				}
+				 else if(temp== "0")
+				{
+					f3 = true;
+					f2 = false;
+					f1 = false;
+					
+				}
+				if(f1)
+				{
+					this.fm2.b5.setLabel("1");
+				}
+				else if(f2)
+				{
+					this.fm2.b5.setLabel("0");
+				}
+				else if(f3)
+				{
+					this.fm2.b5.setLabel("X");
+				}
 			}
 			
-		});
-	}
+		temp2 = this.fm2.b5.getLabel();
+		if(temp2 == "1" )
+		{
+			this.fm2.t1.setText("A");
+		}
+		
+				
+			
 		
 		
 		
 		
-		
-		
-		
-	
+		}
 }
+	
+	
