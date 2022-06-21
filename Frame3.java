f6c023e9c78bcb366285726bc038724f219bc2f7
ArpitAgravat <92981201+ArpitAgravat@users.noreply.github.com>
2022-06-21 23:37:29 +0530

    Update Frame3.java

--- Frame3.java
+++ Frame3.java
@@ -4,7 +4,7 @@ import java.awt.AWTEvent.*;
 import java.util.regex.*;
 class Frame3 extends Frame
 {
-	Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12;
+	Button R,S,V,W,P,Q,T,U,R0,R1,R2;
 	TextField t1,t2;
 	Label l1,l2,l3,l4,l5,l6,l7,lm,ln;
 	MyActionListener L1 = new MyActionListener(this);
@@ -16,45 +16,42 @@ class Frame3 extends Frame
 		t1 = new TextField();
 			t2 = new TextField();
 	
-		b10 = new Button("Enter");
-		b11 = new Button("Clear");
-		b12 = new Button("Back");
+		R0 = new Button("Enter");
+		R1 = new Button("Clear");
+		R2 = new Button("<-Back");
 		
 		 lm = new Label("Max");
 		 ln = new Label("Min");
 		
-		 b1 = new Button("");
-		 b2 = new Button("");
-		 b3 = new Button("");
-		 b4 = new Button("");
-		 b5 = new Button("");
-		 b6 = new Button("");
-		 b7 = new Button("");
-		 b8 = new Button("");
-		 b9 = new Button("");
+		 R = new Button("");
+		 S = new Button("");
+		 V = new Button("");
+		 W = new Button("");
+		 P = new Button("");
+		 Q = new Button("");
+		 T = new Button("");
+		 U = new Button("");
 		 
-		  l1 = new Label("A'");
-		 l2 = new Label("A");
-		  l3= new Label("B'C'");
-		 l4= new Label("B'C");
-		 l5 = new Label("BC");
-		 l6 = new Label("BC'");
+		 l1 = new Label("C'");
+		 l2 = new Label("C");
+		 l3= new Label("A'B'");
+		 l4= new Label("A'B");
+		 l5 = new Label("AB");
+		 l6 = new Label("AB'");
+		
+		P.setBounds(300,300,50,50);//P
+		Q.setBounds(350,300,50,50);//Q
+		R.setBounds(400,300,50,50);//R
+		S.setBounds(450,300,50,50);//S
+		T.setBounds(300,350,50,50);//T
+		U.setBounds(350,350,50,50);//U
+		V.setBounds(400,350,50,50);//V
+		W.setBounds(450,350,50,50);//W
 		
 		
-		b1.setBounds(400,300,50,50);//2->5->6
-		b2.setBounds(450,300,50,50);//5->6
-		b3.setBounds(400,350,50,50);
-		b4.setBounds(450,350,50,50);
-		 b5.setBounds(300,300,50,50);//1
-		 b6.setBounds(350,300,50,50);//1->2
-		// b7.setBounds(100,100,50,50);//3->4
-		 b8.setBounds(300,350,50,50);
-		 b9.setBounds(350,350,50,50);
-		
-		
-		b10.setBounds(350,500,75,55);
-		b11.setBounds(625,500,75,55);
-		b12.setBounds(925,500,75,55);
+		R0.setBounds(350,500,75,55);
+		R1.setBounds(625,500,75,55);
+		R2.setBounds(925,500,75,55);
 		
 		t1.setBounds(700,300,200,40);
 		t2.setBounds(700,350,200,40);
@@ -72,18 +69,17 @@ class Frame3 extends Frame
 		
 		Font f1 = new Font("Arial",Font.BOLD,20);
 		
-		b1.setFont(f1);
-		b2.setFont(f1);
-		b3.setFont(f1);
-		b4.setFont(f1);
-		b5.setFont(f1);
-		b6.setFont(f1);
-		b7.setFont(f1);
-		b8.setFont(f1);
-		b9.setFont(f1);
-		b10.setFont(f1);
-		b11.setFont(f1);
-		b12.setFont(f1);
+		R.setFont(f1);
+		S.setFont(f1);
+		V.setFont(f1);
+		W.setFont(f1);
+		P.setFont(f1);
+		Q.setFont(f1);
+		T.setFont(f1);
+		U.setFont(f1);
+		R0.setFont(f1);
+		R1.setFont(f1);
+		R2.setFont(f1);
 		
 		lm.setFont(f1);
 		ln.setFont(f1);
@@ -95,19 +91,18 @@ class Frame3 extends Frame
 		l5.setFont(f1);
 		l6.setFont(f1);
 		
-		add(b1);
-		add(b2);
-		add(b3);
-		add(b4);
-		add(b5);
-		add(b6);
-		add(b7);
-		add(b8);
-		add(b9);
+		add(R);
+		add(S);
+		add(V);
+		add(W);
+		add(P);
+		add(Q);
+		add(T);
+		add(U);
 		
-		add(b10);
-		add(b11);
-		add(b12);
+		add(R0);
+		add(R1);
+		add(R2);
 		
 		add(t1);
 		add(t2);
@@ -120,19 +115,19 @@ class Frame3 extends Frame
 		add(l6);
 		add(ln);
 		add(lm);
-		
-		 b1.addActionListener(L1);
-		 b2.addActionListener(L1);
-		 b3.addActionListener(L1);
-		 b4.addActionListener(L1);
-		 b5.addActionListener(L1);
-		 b6.addActionListener(L1);
-		 b7.addActionListener(L1);
-		 b8.addActionListener(L1);
-		 b9.addActionListener(L1);
-		b10.addActionListener(L1);
-		b11.addActionListener(L1);
-		b12.addActionListener(L1);
+		setBackground(Color.GRAY);
+		setForeground(Color.BLACK);
+		 R.addActionListener(L1);
+		 S.addActionListener(L1);
+		 V.addActionListener(L1);
+		 W.addActionListener(L1);
+		 P.addActionListener(L1);
+		 Q.addActionListener(L1);
+		 T.addActionListener(L1);
+		 U.addActionListener(L1);
+		R0.addActionListener(L1);
+		R1.addActionListener(L1);
+		R2.addActionListener(L1);
 		
 		addWindowListener(new WindowAdapter()
 		{
